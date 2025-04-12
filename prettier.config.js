15cf18df084ba5a1569cb82f335489d2761fd366
Tedy Gabriel Moisa <moisatedy@gmail.com>
2025-04-12 18:07:31 +0200

    init: project initialized

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: "none",
+  singleAttributePerLine: false
+};
